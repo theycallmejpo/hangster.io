1f82ea5622501b24d90bb5ab485cbd16beecac17
Juan Posadas <jposadas@stanford.edu>
2014-09-09 12:33:06 +0200

    added validation and styling

--- website/js/signup.js
+++ website/js/signup.js
@@ -2,17 +2,21 @@
 
 	$(document).ready(function() {
 
+		/*Validation Code */
+		var isEmail = function (email) {
+  			var regex = /^([a-zA-Z0-9_.+-])+\@(([a-zA-Z0-9-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+  			return regex.test(email);
+		}
+		
 		$('#email-signup').click(function(event) {
 			event.preventDefault();
 			var signup = new Firebase("https://hangster-launch.firebaseio.com/signup/dev");
 			var email = $('.email-input').val();
 
-			if (email) {
-				signup.push({
-					'email': email
-				});
+			if (isEmail(email)) {
+				signup.push({ 'email': email });
 			} else {
-				console.debug('No email written');
+				console.debug('Email not well formatted');
 			}
 			
 		});
